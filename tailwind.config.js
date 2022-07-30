3c9765e0468649f67db7c3dc9a8bf75e5196191e
Laura Ohrndorf <laura.ohrndorf@googlemail.com>
2022-07-30 15:02:58 +0000

    Add resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,44 @@ module.exports = {
         'sandy-brown': '#F4A261',
         'burnt-sienna': '#E76F51'
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            h2: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.900'),
+            },
+            h3: {
+              fontWeight: '600',
+              color: theme('colors.gray.900'),
+            },
+            'ol li:before': {
+              fontWeight: '600',
+              color: theme('colors.gray.500'),
+            },
+            'ul li:before': {
+              backgroundColor: theme('colors.gray.400'),
+            },
+            code: {
+              color: theme('colors.gray.900'),
+            },
+            a: {
+              color: theme('colors.gray.900'),
+              fontWeight: 400,
+            },
+            pre: {
+              color: theme('colors.white'),
+              backgroundColor: theme('colors.gray.800'),
+            },
+            blockquote: {
+              color: theme('colors.gray.900'),
+              borderLeftColor: theme('colors.gray.200'),
+            },
+          },
+        },
+      })
     },
   },
   variants: {},
